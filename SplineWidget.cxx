1ccddedf131dc8c5640153e30a5e1c7232c6f828
parjapat22 <bsntparjapat8@gmail.com>
2015-07-21 12:04:05 +1000

    Fixed reading a spine

--- SplineWidget.cxx
+++ SplineWidget.cxx
@@ -9,7 +9,6 @@
 #include <fstream>
 #include <string>
 
-
 int main(int, char *[])
 {
   // A renderer and render window
@@ -31,38 +30,89 @@ int main(int, char *[])
 
   vtkSmartPointer<vtkSplineRepresentation> rep1 = vtkSmartPointer<vtkSplineRepresentation>::New();
   rep1->SetHandleSize(9);
-  rep1->SetNumberOfHandles(5);
-  double position[3];
-  position[0] = 0;
-  position[1] = 0.8;
-  position[2] = 0.1;
-  rep1->SetHandlePosition(0,position);
-
-  position[0] = 0;
-  position[1] = 0.65;
-  position[2] = 0.17;
-  rep1->SetHandlePosition(1, position);
-
-  position[0] = 0;
-  position[1] = 0.5;
-  position[2] = 0.32;
-  rep1->SetHandlePosition(2, position);
-
-  position[0] = 0;
-  position[1] = 0.32;
-  position[2] = 0.52;
-  rep1->SetHandlePosition(3, position);
-
-  position[0] = 0;
-  position[1] = 0.2;
-  position[2] = 0.9;
-  rep1->SetHandlePosition(4, position);
   
+ 
+  using namespace std;
+  ifstream file;
+  file.open("SplineSpine.txt");
+  string word;
+  double position[3];
+
+  if (!file.is_open())
+  {
+	  rep1->SetNumberOfHandles(5);
+	  position[0] = 0;
+	  position[1] = 0.8;
+	  position[2] = 0.1;
+	  rep1->SetHandlePosition(0, position);
+
+	  position[0] = 0;
+	  position[1] = 0.65;
+	  position[2] = 0.17;
+	  rep1->SetHandlePosition(1, position);
+
+	  position[0] = 0;
+	  position[1] = 0.5;
+	  position[2] = 0.32;
+	  rep1->SetHandlePosition(2, position);
+
+	  position[0] = 0;
+	  position[1] = 0.32;
+	  position[2] = 0.52;
+	  rep1->SetHandlePosition(3, position);
+
+	  position[0] = 0;
+	  position[1] = 0.2;
+	  position[2] = 0.9;
+	  rep1->SetHandlePosition(4, position);
+
+	  cout << "File not found, output by default \n";
+  }
+  else
+  {
+	  // Read the first word from the file
+	  file >> word;
+	  // Check if it matches the keyword
+	  if (word.compare("SplineSpine"))
+	  {
+		  cout << "ERROR when reading input file - not the correct format." << endl;
+		  return EXIT_FAILURE;
+	  }
+	  else
+	  {
+		  cout << "Input file is correct." << endl;
+	  }
+
+	  // Read the second word from the file - number of handles
+	  file >> word;
+	  int numberOfHandles = atoi(word.c_str());
+	  cout << "Number of Handles: " << numberOfHandles << endl <<endl;
+	  rep1->SetNumberOfHandles(numberOfHandles);
+	  
+	  for (int i = 0; i < numberOfHandles; i++)
+	  {
+		  file >> word;
+		  double x = std::stod(word);
+
+		  file >> word;
+		  double y = std::stod(word);
+
+		  file >> word;
+		  double z = std::stod(word);
+		 
+		  cout << x <<" "<< y <<" "<< z << endl;
+		  rep1->SetHandlePosition(i, x, y, z);
+	  }
+	  cout<<"Read Successfully \n"<<endl;
+    }
+
   /*
   vtkSmartPointer<vtkProperty> colorProperty;
   colorProperty->SetColor(0.5, 0.5, 0.5);
   rep1->SetHandleProperty(colorProperty);
   */
+
+  rep1->ProjectToPlaneOn();
 
   splineWidget->SetRepresentation(rep1);
   //splineWidget->GetRepresentation()->SetHandleSize(10);
@@ -80,30 +130,17 @@ int main(int, char *[])
 	  std::cout << i << ": " << position[0] << " " << position[1] << " " << position[2] << std::endl;
   }
 
-  using namespace std;
-    
-  ifstream file("SplineSpine.txt");
-  string content;
-  if (file.is_open())
-  {
-	  while (getline(file, content))
-	  {
-		  cout << content << "\n";
-	  }
-	  cout << "Read Successfully \n \n";
-	  file.close();
-  }
-  else
-	  cout << "Unable to open file \n \n";
-
   ofstream myfile;
   myfile.open("SplineSpine.txt");
-  myfile << "\n SplineSpine" << std::endl<<"\n";
+  myfile <<"SplineSpine" << std::endl;
+  myfile << rep1->GetNumberOfHandles() << std::endl;
+  
   for (int i = 0; i<rep1->GetNumberOfHandles(); ++i)
   {
 	  rep1->GetHandlePosition(i, position);
-	  myfile << i << ": " << position[0] << "  " << position[1] << "  " << position[2] << std::endl<<"\n";
+	  myfile << position[0] << "  " << position[1] << "  " << position[2] << std::endl;
   }
+  cout<<"Written Successfully\n\n";
   myfile.close();
   
   return EXIT_SUCCESS;
